0fffac3083d25c993cb248c0487f40f459b2cf47
akshatarora130 <akshatarora130@gmail.com>
2023-10-24 02:06:42 +0530

    removed the dist directory

--- server/dist/info/mongooseConnect.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongooseUrl = void 0;
-exports.mongooseUrl = "mongodb+srv://akshatarora0013:Ravi123Sapna@courseselling.9ueqmee.mongodb.net/";
--- server/dist/info/port.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.port = void 0;
-exports.port = 3000;
