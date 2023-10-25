af629fd0ab30602792304d67c1194ea7a90e3adf
akshatarora130 <akshatarora130@gmail.com>
2023-10-26 02:24:40 +0530

    added checkAdminStatus miidleWare

---
+++ server/src/middlewares/checkAdminStatus.ts
@@ -0,0 +1,12 @@
+import {Request, Response, NextFunction} from "express";
+import {Chat} from "../databaseModels/chatModel";
+
+export const checkAdminStatus = async (req: Request, res: Response, next: NextFunction) => {
+    const { chatId } = req.body;
+    const chat = await Chat.findOne({ _id: chatId, groupAdmin: req.user?._id });
+
+    if (!chat) {
+        return res.status(403).json({ message: 'Permission denied. You are not the group admin.' });
+    }
+    next();
+}
