dfd2320941625e4ee8cc1f61d06bc2ab67e77ed5
akshatarora130 <akshatarora130@gmail.com>
2023-10-27 20:58:48 +0530

    modified fetchMessages Route

--- server/src/routes/messages/fetchMessages.ts
+++ server/src/routes/messages/fetchMessages.ts
@@ -1,18 +1,35 @@
-import express, {Router} from "express";
-import {authenticateJWT} from "../../middlewares/authenticateJWT";
-import {Message} from "../../databaseModels/messageModel";
+import express, { Router } from "express";
+import { authenticateJWT } from "../../middlewares/authenticateJWT";
+import { Message } from "../../databaseModels/messageModel";
 
 const router: Router = express.Router();
 
 router.get("/fetchMessages/:chatId", authenticateJWT, async (req, res) => {
-    try{
-        const messages = await Message.find({chat: req.params.chatId}).populate("sender", "name pic email").populate("chat");
+    try {
+        const messages = await Message.find({ chat: req.params.chatId })
+            .populate({
+                path: "sender",
+                select: "name profilePic email",
+            })
+            .populate("chat")
+            .populate({
+                path: "chat",
+                populate: [
+                    {
+                        path: "users",
+                        select: "name profilePic email",
+                    },
+                    {
+                        path: "latest",
+                    },
+                ],
+            });
+
         res.json(messages);
-    }
-    catch (error){
+    } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal Server Error' });
     }
-})
+});
 
 export default router;
